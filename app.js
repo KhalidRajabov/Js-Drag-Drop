fddc8e4e2b017cecbe5cc1118d8d6674d7c173ad
dbBurzum <khalidsr@code.edu.az>
2022-05-26 17:43:42 +0400

    described

--- app.js
+++ app.js
@@ -1,36 +1,73 @@
 const draggable = document.querySelectorAll(".draggable")
 const containers = document.querySelectorAll(`.container`)
 
-// draggable.forEach(item=>{
-//     item.addEventListener("dragstart")
-//     console.log("started");
-// })
-
-
-
 draggable.forEach(draggable=>{
-    draggable.addEventListener("dragstart", ev=>{
-    console.log("dragging")
-    
+    draggable.addEventListener("dragstart", _=>{
+        
+
+    //Sürüşdürdüyüm elementin opasitisini azaldacaq bir class verirəm ki 
+    //bilinsin hansı elementi aparıram.    
     draggable.classList.add(`dragging`)
 })
-    draggable.addEventListener("dragend", _=> draggable.classList.remove(`dragging`))
+
+    draggable.addEventListener("dragend", _=> {
+
+        //Drag bitəndə opasitini də silirəm.
+        draggable.classList.remove(`dragging`)
+    })
 })
 
 containers.forEach(container=>{
     container.addEventListener("dragover", e=>{
         e.preventDefault();
+        //üstündə olduğum konteynerin bilinməsi
+        //üçün class verirəm
         container.classList.add("dragover")
+
+        //sürüşdürməkdə olduğum elementi seçirəm
+        const draggable = document.querySelector('.dragging')
+
+        //sürüşdürdüyüm elementin ilə konteynerin
+        //idlərinin eyni olub olmadğını yoxlayıram.
+        let dragged = draggable.getAttribute("id")
+        let contId = container.getAttribute("id")
+        if (parseInt(dragged)!=parseInt(contId)) {
+
+            //əgər element ilə konteynerin idsi bərabər 
+            //deyilsə onda konteynerə qırmızı
+            //arxa fon verirəm ki başa düşülsün
+            //ki o element bura getməz
+            container.classList.add("red")
+        }
     })
     container.addEventListener("drop", _=>{
+        //yenidən sürüşən elementi, onun idsini və
+        //konteynerin idsini götürürəm
         const draggable = document.querySelector('.dragging')
         let dragged = draggable.getAttribute("id")
         let contId = container.getAttribute("id")
+
+
+        //bir birinə bərabərdirlərsə onda 
+        //içinə atmaq olar
         if (parseInt(dragged)==parseInt(contId)) {
             container.append(draggable)
+            container.classList.remove("dragover")
         }
+
+        
         
     })
-    container.addEventListener("dragleave", _=>container.classList.remove("dragover"))
+    //və əgər içəri atmaq olmazsa və
+    //curson konteynerdən kənara çıxarsa 
+    //onda konteyneri seçərkən yaranan
+    // arxa fonu və elementi konteynerə ata
+    // bilmədiyimiz zaman yaranan 
+    //qırmızı arxa fonu silirik
+    
+    container.addEventListener("dragleave", _=>{
+        container.classList.remove("dragover")
+        container.classList.remove("red")
+    })
     
 })
